8c525701bac46ddf699bd0cd8f60156b4b22e969
EugeneChebykin <respublicofrome@gmail.com>
2019-12-30 14:11:14 +0300

    Updates

---
+++ precache-manifest.6477c8fe6772bfc9462b63a3ebbe3701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b41ac4da78222c5f949ca8e488284b3",
+    "url": "/Stopwatch_and_countdown/index.html"
+  },
+  {
+    "revision": "862aa0dd349f74a7885e",
+    "url": "/Stopwatch_and_countdown/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "1024e6ed8578352a4f4d",
+    "url": "/Stopwatch_and_countdown/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "862aa0dd349f74a7885e",
+    "url": "/Stopwatch_and_countdown/static/js/2.ca0c459d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Stopwatch_and_countdown/static/js/2.ca0c459d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1024e6ed8578352a4f4d",
+    "url": "/Stopwatch_and_countdown/static/js/main.0c5a1f7d.chunk.js"
+  },
+  {
+    "revision": "de8c77638a9a3dcd2ea2",
+    "url": "/Stopwatch_and_countdown/static/js/runtime-main.b286ea50.js"
+  },
+  {
+    "revision": "fce0c8033c5fcd21f5a6fcef75452e49",
+    "url": "/Stopwatch_and_countdown/static/media/ding.fce0c803.mp3"
+  }
+]);
